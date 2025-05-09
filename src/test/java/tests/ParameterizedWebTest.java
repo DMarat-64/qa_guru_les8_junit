1a6beac13de427be7f06df8b0c14051190a9b942
Marat D <Marat@mail.ru>
2025-05-09 13:58:13 +0400

    added Cav.test

--- src/test/java/tests/ParameterizedWebTest.java
+++ src/test/java/tests/ParameterizedWebTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ParameterizedWebTest {
@@ -14,22 +17,32 @@ public class ParameterizedWebTest {
     void setUp() {
         open("https://duckduckgo.com/");
     }
+
     @ValueSource(strings = {
             "Idea",
             "Selector",
             "Allure"
     })
 
-
     @ParameterizedTest(name = "Для поискового запроса {0} не должен выдаваться пустой результат")
     @Tags({
             @Tag("WEB"),
             @Tag("Regress")
     })
-    void successfulSearchTest(String searchQuery) {
+    void searchResultsShouldNotBeEmpty(String searchQuery) {
         $("#searchbox_input").setValue(searchQuery).pressEnter();
         $$("[data-testid='mainline'] li[data-layout='organic']")
                 .shouldBe(sizeGreaterThan(0));
     }
+
+    @CsvFileSource(resources = "/test_data/searchResultsShouldContainExpectedUrl.csv")
+    @ParameterizedTest(name = "Для поискового запроса {0} в первой карточке должна быть ссылка {1}")
+    @Tag("Regress")
+    void searchResultsShouldContainExpectedUrl(String searchQuery, String expectedLink) {
+        $("#searchbox_input").setValue(searchQuery).pressEnter();
+        $("[data-testid='mainline'] li[data-layout='organic']")
+                .shouldHave(text(expectedLink));
+    }
+
 
 }
